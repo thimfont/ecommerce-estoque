f2ba5722f9f0350816a386bf2d1e199ab515dc88
Thiago <thimfont@gmail.com>
2023-04-29 21:36:09 -0300

    feat: adiciona log na consulta de produto.

--- src/main/java/com/br/estoque/EstoqueService.java
+++ src/main/java/com/br/estoque/EstoqueService.java
@@ -2,21 +2,28 @@ package com.br.estoque;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EstoqueService {
-
+    private Logger log = LoggerFactory.getLogger(EstoqueController.class);
     @Autowired
     private ProdutoRepository repository;
 
     //TODO: criar uma service para lidar com metodos do repository e assim mover a exception para ele evitando duplicar codigo
     @Transactional
     public EstoqueResponse reservaSeProdutoDisponivel(Long produtoId, int quantidadeNecessaria) {
+        log.info("verifica se produtoId: " + produtoId + " possui a quantidade de: " + quantidadeNecessaria + " disponivel");
+
         Produto produto = localizaProduto(produtoId);
         boolean estaDisponivel = possuiQuantidadeNecessaria(produto.getQuantidade(), quantidadeNecessaria);
-        return reservaProduto(estaDisponivel, produto, quantidadeNecessaria);
+        EstoqueResponse estoqueResponse = reservaProduto(estaDisponivel, produto, quantidadeNecessaria);
+
+        log.info("produtoId: " + produtoId + " esta disponivel: " + estoqueResponse.isEstaDisponivel());
+        return estoqueResponse;
     }
 
     private Produto localizaProduto(Long produtoId) {
